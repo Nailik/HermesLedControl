7b62b53bf88c88abe41047de68ff96f9bb093216
Nailik <kilianeller12@outlook.com>
2022-02-12 19:59:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ echo "virtualenv -p ${PYTHON} ${FVENV} source ${FVENV}/bin/activate pip install 
 sudo -u "${USER}" bash <<EOF
     virtualenv -p ${PYTHON} ${FVENV}
     source ${FVENV}/bin/activate
-    pip3 install -r requirements.txt --no-cache-dir
+    pip3 install --index-url=http://pypi.python.org/simple/ --trusted-host pypi.python.org -r requirements.txt --no-cache-dir
 EOF
 #next test py -3.7 -m pip install -r numpy --no-cache-dir -> py not found
 
